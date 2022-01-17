d1f55838e077a24e43b603b312cad42b9c63edfe
Dahhan_coder <moustafaeldahhan@gmail.com>
2022-01-17 12:57:29 -0500

    testing node

---
+++ app.js
@@ -0,0 +1,23 @@
+//slice returns everything from the returns everything from and including the first argument up until but not including the second argument
+const profileDataArgs = process.argv.slice(2, process.argv.legnth);
+console.log(profileDataArgs)
+const animalArray= ["dog", "cat", "bird"]
+animalArray.push('cow')
+const personObj ={name: "Moustafa", age:"23"};
+personObj.age= 24;
+personObj.occupation= "developer";
+personObj.cell="+1(905)-464-7718"
+personObj.Email="moustafaeldahhan@gmail.com"
+console.log(personObj)
+//Arrow functions replace the normal function statement
+//Arrow function can't be used as the main functions
+const printProfileData = (profileDataArr) => {
+    profileDataArr.forEach((profileItem) => {
+        console.log(profileItem)
+    });
+    };
+printProfileData([personObj]);
+//example
+const addNums =(numOne,numTwo) => numOne + numTwo;
+const sum = addNums(2,5);
+console.log(sum)
